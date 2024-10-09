c11a2b0c3f5641b707e5b3570ebcc6516c91c455
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-10-09 14:40:02 -0300

    activity 5 exercise 2 with threads

--- activity-5/punto-2.c
+++ activity-5/punto-2.c
@@ -6,77 +6,73 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+
 #define A 1
 #define B 2
 #define C 3
+
+int CANT_ITERACIONES = 5;
 
 typedef struct {
     long mtype;  // Tipo de mensaje
 } mensaje;
 
-void procesoA(int cola_id, int reps) {
+void procesoA(int cola_id) {
     mensaje msg;
-    for (int i = 0; i < reps * 4; i++) {
+    for (int i = 0; i < CANT_ITERACIONES * 4; i++) {
         // Esperar mensaje de tipo A
         msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), A, 0);
 
-        // Imprimir A
         printf("A");
-	fflush(stdout);
+	    fflush(stdout);
         // Enviar mensaje de tipo C
         msg.mtype = C;
         msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+        sleep(1);
     }
 }
 
-void procesoB(int cola_id, int reps) {
+void procesoB(int cola_id) {
     mensaje msg;
-    for (int i = 0; i < reps * 3; i++) {
+    for (int i = 0; i < CANT_ITERACIONES * 3; i++) {
         // Esperar mensaje de tipo B
         msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), B, 0);
 
         // Imprimir B
         printf("B");
-	fflush(stdout);
-        // Enviar mensaje de tipo A
+	    fflush(stdout);
         msg.mtype = A;
         msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+        sleep(1);
     }
 }
 
-void procesoC(int cola_id, int reps) {
+void procesoC(int cola_id) {
     mensaje msg;
-    for (int i = 0; i < reps; i++) {
-        // Esperar y enviar mensajes 4 veces
+    for (int i = 0; i < CANT_ITERACIONES; i++) {
         
-            msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
-            msg.mtype = B;
-            msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+        msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
+        msg.mtype = B;
+        msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
 		msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
-            msg.mtype = B;
-            msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+        msg.mtype = B;
+        msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
 
-	msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
-            msg.mtype = B;
-            msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
-	msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
+	    msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
+        msg.mtype = B;
+        msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+	    msgrcv(cola_id, &msg, sizeof(msg) - sizeof(long), C, 0);
 
-	// Imprimir C
-        printf("C");
-	fflush(stdout);
+        printf("C\n");
+	    fflush(stdout);
         // Enviar mensaje de tipo A
         msg.mtype = A;
         msgsnd(cola_id, &msg, sizeof(msg) - sizeof(long), 0);
+        sleep(1);
     }
 }
 
 int main() {
-    int repeticiones;
-
-    do {
-        printf("\nIngrese la cantidad de repeticiones: ");
-        scanf("%d", &repeticiones);
-    } while (repeticiones <= 0);
 
     // Crear cola de mensajes
     int cola_id = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
@@ -86,23 +82,20 @@ int main() {
     }
 
     // Crear procesos
-    pid_t pidA = fork();
-    if (pidA == 0) {
-	
-        procesoA(cola_id, repeticiones);
-        exit(0);
-    }
-
-    pid_t pidB = fork();
-    if (pidB == 0) {
-        procesoB(cola_id, repeticiones);
-        exit(0);
-    }
-
-    pid_t pidC = fork();
-    if (pidC == 0) {
-        procesoC(cola_id, repeticiones);
-        exit(0);
+    pid_t pidA, pidB, pidC;
+    pidA = fork();
+    if (pidA == 0) {	
+        procesoA(cola_id);
+    } else {
+        pidB = fork();
+        if (pidB == 0) {
+            procesoB(cola_id);
+        } else {
+            pidC = fork();
+            if (pidC == 0) {
+                procesoC(cola_id);
+            }
+        }
     }
 
     // Inicializar la secuencia enviando el primer mensaje a A
@@ -117,8 +110,6 @@ int main() {
 
     // Eliminar la cola de mensajes
     msgctl(cola_id, IPC_RMID, NULL);
-
-    printf("\nLa secuencia terminó\n");
 
     return 0;
 }
