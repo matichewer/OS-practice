4dd69d99ccfd2db5c549a46e5a32029edf4f6ff6
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-10-22 15:36:32 -0300

    problema de la diapo

---
+++ diapo/pdf_Sincronización-Problemas-1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+// Declaración de semáforos
+sem_t semA, semB, semC;
+
+// Función para imprimir 'A'
+void *printA(void *arg) {
+    while (1) {
+        printf("A"); // PONERLO ACA HACE QUE FUNCIONE MAL
+        fflush(stdout); // YA QUE CICLA 2 VECES AL PRINCIPIO
+
+        sem_wait(&semA); 
+
+        sem_post(&semB); 
+    }
+    return NULL;
+}
+
+// Función para imprimir 'B'
+void *printB(void *arg) {
+    while (1) {
+        sem_wait(&semB);
+
+        printf("B");
+        fflush(stdout);
+    //    sleep(1);
+
+        sem_post(&semC); 
+    }
+    return NULL;
+}
+
+// Función para imprimir 'C'
+void *printC(void *arg) {
+    while (1) {
+        sem_wait(&semC); 
+
+        printf("C\n");
+        fflush(stdout);
+        sleep(1);
+
+        sem_post(&semA); 
+    }
+    return NULL;
+}
+
+int main() {
+    // Declaración de threads
+    pthread_t threadA, threadB, threadC, threadD, threadE;
+
+    // Inicialización de semáforos
+    sem_init(&semA, 0, 1); // A comienza primero
+    sem_init(&semB, 0, 0); // B comienza bloqueado
+    sem_init(&semC, 0, 0); // C comienza bloqueado
+
+    // Creación de hilos
+    pthread_create(&threadA, NULL, printA, NULL);
+    pthread_create(&threadB, NULL, printB, NULL);
+    pthread_create(&threadC, NULL, printC, NULL);
+
+    // Espera a que los hilos terminen (esto no ocurrirá en este caso)
+    pthread_join(threadA, NULL);
+    pthread_join(threadB, NULL);
+    pthread_join(threadC, NULL);
+
+    // Destrucción de semáforos (esto no se ejecutará)
+    sem_destroy(&semA);
+    sem_destroy(&semB);
+    sem_destroy(&semC);
+
+    return 0;
+}
